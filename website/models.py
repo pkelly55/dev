03eddfc3d6d1babf2f65e603323f9b63089e5201
Peyton Kelly <pkelly5511@gmail.com>
2024-03-13 12:51:56 -0400

    Add state to be tracked

--- website/models.py
+++ website/models.py
@@ -34,7 +34,7 @@ class Monitor(models.Model):
     capital = models.CharField(max_length=50, blank=True, null=True)
     datetime = models.DateField(max_length=50, blank=True, null=True)
     ip = models.CharField(max_length=50, blank=True, null=True)
-
+    state= models.CharField(max_length=50, blank=True, null=True)
     def __str__(self):
         return self.ip
     
